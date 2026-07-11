d83a2db44c576efb64102f756e8f6f8a8a717f5e
Rediet Sharew <redietsharew231@gmail.com>
2026-07-11 11:05:52 +0300

    setup backend

---
+++ backend/src/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+// Test route
+app.get("/", (req, res) => {
+    res.send("Song Management API is running");
+});
+
+
+// Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
